4e4a4ae55b442806aee101a5ec305e89ca31261a
Patel-Phaneendra <99329272+Patel-Phaneendra@users.noreply.github.com>
2022-06-24 16:37:34 +0530

    Update main.go
    Hello world updated

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func main() {
 func handler(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	if name == "" {
-		name = "from Bangalore"
+		name = "Hello World"
 	}
 	fmt.Fprintf(w, "Hello %s!\n", name)
 }
